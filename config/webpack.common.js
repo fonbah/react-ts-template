82e9f315be5e57de785bf2d169bae9faf71c9240
fonbah <valerijcezij@gmail.com>
2021-06-06 14:01:54 +0200

    React TS

---
+++ config/webpack.common.js
@@ -0,0 +1,54 @@
+'use strict'
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/main.tsx',
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist/public'),
+        filename: '[name].[contenthash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'dist/template.html',
+            title: 'React TS template',
+        }),
+    ]
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import Component from './components/Component';
+
+const App: FC = () => (
+    <Component content={'Component text!'} />
+);
+
+export default App;
