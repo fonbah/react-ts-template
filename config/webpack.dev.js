87839788bc239c6303ea2cbac0a72d9927ac24cf
fonbah <valerijcezij@gmail.com>
2021-06-12 17:14:53 +0200

    webpack HMR

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,26 @@
+const webpack = require('webpack');
+const webPackdevServer = require('webpack-dev-server');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+const config = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist/public',
-    }
+});
+
+const options = {
+    contentBase: './dist/public',
+    hot: true,
+    host: 'localhost',
+    open: true,
+    clientLogLevel: 'error',
+};
+
+webPackdevServer.addDevServerEntrypoints(config, options);
+
+const compiler = webpack(config);
+const server = new webPackdevServer(compiler, options);
+
+server.listen(8080, 'localhost', () => {
+    console.log('listening on port 8080');
 });
