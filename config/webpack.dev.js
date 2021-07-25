7aed521716ddefc302becbd682f1832fc7ebd1b2
fonbah <valerijcezij@gmail.com>
2021-07-25 13:47:20 +0200

    Config restored

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,29 +1,13 @@
-const webpack = require('webpack');
-const webPackdevServer = require('webpack-dev-server');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const config = merge(common, {
+module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
         filename: 'app.js',
     },
-});
-
-const options = {
-    contentBase: './dist/public',
-    host: 'localhost',
-    open: true,
-    clientLogLevel: 'error',
-    historyApiFallback: true,
-};
-
-webPackdevServer.addDevServerEntrypoints(config, options);
-
-const compiler = webpack(config);
-const server = new webPackdevServer(compiler, options);
-
-server.listen(8080, 'localhost', () => {
-    console.log('listening on port 8080');
+    devServer: {
+        contentBase: './dist/public',
+    }
 });
